163346939e8e18d206ce58359d5b87ef094ef930
eoce <eoce@msn.com>
2023-08-31 17:29:16 +0800

    Initial commit

--- src/views/DataDirectory.vue
+++ src/views/DataDirectory.vue
@@ -45,6 +45,7 @@
                      @mouseup="endDrag"
                      @mouseleave="endDrag">
             </div>
+            123
             <td>{{ item.id }}</td>
             <td>{{ item.zname }}</td>
             <td>{{ item.ProductTime }}</td>
